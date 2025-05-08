b04cff15e8aa0fee59e068393190ae703c34a8fa
GustavoAdolfoNavarroPacheco <adolfonavarro0212@gmail.com>
2025-05-07 23:54:50 -0500

    feat:  :memo: Folder Order and New Files

---
+++ Python_Ejercicios_Dia2/Punto4/ArithmeticOperators_HackerRank_NavarroGustavo.py
@@ -0,0 +1,15 @@
+# Entrada: 2 numeros ingresados por el usuario
+# Salida: Imprimir Suma, Resta y Multiplicacion de los numeros
+
+num1=int(input('Ingrese el primer numero: '))
+num2=int(input('Ingrese el segundo numero: '))
+
+total = num1 + num2
+diferencia = num1 - num2
+producto = num1 * num2
+
+print(total)
+print(diferencia)
+print(producto)
+
+#Desarrollado por Gustavo Adolfo Navarro Pacheco T.I 1.127.599.969
---
+++ Python_Ejercicios_Dia2/Punto4/Division_HackerRank_NavarroGustavo.py
@@ -0,0 +1,13 @@
+# Entrada: Numeros a dividir
+# Salida: Cociente de la division
+
+num1 = int(input('Ingresa el primer numero'))
+num2 = int(input('Ingresa el segundo numero'))
+
+cociente = num1 / num2
+cociente2 = num1 // num2 
+
+print(cociente)
+print(cociente2)
+
+#Desarrollado por Gustavo Adolfo Navarro Pacheco T.I 1.127.599.969
